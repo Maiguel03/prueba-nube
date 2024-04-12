9c09aec67f39992c379bb5aa486204d70a5dcbef
Maiguel03 <115935567+Maiguel03@users.noreply.github.com>
2024-04-12 15:43:03 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,56 +1,19 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"time"
+	"prueba/handlers"
 
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
 )
 
-type usuario struct {
-	User string 
-	Pass string 
-}
 
-func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS") // Especifica los métodos permitidos
-	(*w).Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept") // Especifica las cabeceras permitidas
-}
-
-func Login(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	enableCors(&w)
-	if r.Method == "POST" {
-		User := usuario{}
-		json.NewDecoder(r.Body).Decode(&User)
-		json.NewEncoder(w).Encode(User)
-		fmt.Println(User)
-		return
-	}
-
-	if r.Method == "GET" {
-		User := usuario{User: "miguel", Pass: "1234"}
-		json.NewEncoder(w).Encode(User)
-	}
-}
 
 func main() {
-	router := mux.NewRouter()
+	r := gin.Default()
 
-	router.HandleFunc("/", Login)
+	r.POST("/registro", handlers.Registro) 
+	r.POST("/login", handlers.Login)
+	r.GET("/validar", handlers.Validar)
+	r.Run(":8080")
 
-	server := http.Server{
-		Addr:              ":8080",
-		Handler:           router,
-		ReadTimeout:       30 * time.Second,
-		ReadHeaderTimeout: 30 * time.Second,
-		WriteTimeout:      30 * time.Second,
-		IdleTimeout:       30 * time.Second,
-	}
-
-	server.ListenAndServe()
 }
-
