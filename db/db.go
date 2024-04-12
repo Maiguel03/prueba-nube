180949bff9df22e837c99a0ab47784fc84b815ef
Maiguel03 <115935567+Maiguel03@users.noreply.github.com>
2024-04-12 15:42:30 -0400

    Create db.go

---
+++ db/db.go
@@ -0,0 +1,70 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"prueba/models"
+
+	_ "github.com/lib/pq"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	dbHost := "dpg-cnn7jl6n7f5s73da6f70-a.oregon-postgres.render.com"
+	dbPort := "5432"
+	dbUser := "dek"
+	dbPassword := "rubqwjCMeRfKkwvGsGeYudqOpmKtbo34"
+	dbName := "empanada"
+
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=require",
+		dbHost, dbPort, dbUser, dbName, dbPassword)
+
+	DB, err := sql.Open("postgres", connectionString)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return DB, nil
+}
+
+func CrearUsuario(Usuario models.Usuario) error {
+	db, _ := ConnectDB()
+	defer db.Close()
+	stmt, err := db.Prepare("Insert into Usuario (CI ,Nombre ,Apellido ,Usuario ,Contraseña ,Categoria) values ($1, $2, $3, $4, $5, $6)")
+
+	if err != nil {
+		return err
+	}
+	
+	_, err = stmt.Exec(Usuario.CI, Usuario.Nombre, Usuario.Apellido, Usuario.User, Usuario.Pass, "Administrador")
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Login(Usuario string) (error, string) {
+	db, err := ConnectDB()
+	
+
+	if err != nil {
+		log.Print(err)
+		return err, ""
+	}
+	var HashContraseña string
+	err = db.QueryRow("select Contraseña from usuario where Usuario = $1", Usuario).Scan(&HashContraseña)
+
+	if err != nil {
+		if err == sql.ErrNoRows{
+		log.Print(err)
+		return err, ""
+		} else{
+			log.Print("Error al consulta la base de datos", err)
+			return err, ""
+		}
+	}
+	return nil, HashContraseña
+}
